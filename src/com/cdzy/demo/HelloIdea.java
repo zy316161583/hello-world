b08cfb7df0dbd531ebc829c8895e896ec4ad1a0b
zhangyu <316161583@qq.com>
2017-09-01 16:35:56 +0800

    Initial commit

---
+++ src/com/cdzy/demo/HelloIdea.java
@@ -0,0 +1,20 @@
+package com.cdzy.demo;
+
+
+import org.junit.Test;
+
+/***
+ * 测试helloworld
+ */
+public class HelloIdea {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+    @Test
+    public void test(){
+        System.out.println("haahahah");
+    }
+
+
+}
